8772451bc68211f887120cfc52657c520ea4bfe5
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-16 14:28:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
+require 'date'
+
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
-
+  get "/hello" do
+    erb :hello
+  end
+  
+  get "/goodbye" do
+    @name = "Joe"
+    erb :goodbye
+  end
+  
+  get "/date" do
+    @d = DateTime.now.strftime("The date is %A, %B %d, %Y")
+    erb :date
+  end
 end
